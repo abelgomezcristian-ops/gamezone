9c83d5559e3ff87f500b9fa1f5af51f01e7b7871
abelgomezcristian-ops <abel.gomez.cristian@gmail.com>
2026-09-10 16:33:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const games = [
     {id:1,name:"Grand Theft Auto: San Andreas",category:"destacado",price:2500,genre:"Mundo Abierto",top:true,image:"img/gta-san-andreas.jpg"},
-    {id:2,name:"God of War",category:"destacado",price:2500,genre:"Hack and Slash",top:true},
+    {id:2,name:"God of War",category:"destacado",price:2500,genre:"Hack and Slash",top:true,image:"img/god-of-war.jpg"},
     {id:3,name:"God of War II",category:"destacado",price:2500,genre:"Hack and Slash",top:true},
     {id:4,name:"Shadow of the Colossus",category:"destacado",price:2500,genre:"Aventura",top:true},
     {id:5,name:"Metal Gear Solid 3: Snake Eater",category:"destacado",price:2500,genre:"Sigilo",top:false},
